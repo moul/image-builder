75a0e1f9d1bf1ea6e5aab72c41887420224e925a
Edouard Bonlieu <ebonlieu@ocs.online.net>
2014-10-29 12:44:27 +0100

    Fix mysql install

--- images/app-wordpress/patches/usr/local/bin/install_mysql_server.sh
+++ images/app-wordpress/patches/usr/local/bin/install_mysql_server.sh
@@ -9,13 +9,23 @@ if [ ! -f /root/.my.cnf ]; then
 user = $username
 password = $password
 EOF
-    echo "mysql-server mysql-server/root_password password $password" debconf-set-selections
-    echo "mysql-server mysql-server/root_password_again password $password" debconf-set-selections
-    apt-get install -qy mysql-server
+
+    # Reset mysql root password
+    cat <<EOF > /tmp/recover_root_mysql.ini
+UPDATE mysql.user SET password=PASSWORD("$password") where User='$username';
+FLUSH PRIVILEGES;
+EOF
+    service mysql stop
+    mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
+    sleep 5
+    echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
+    killall mysqld
+    rm -f /tmp/recover_root_mysql.ini
+    server mysql start
+
+    # Configure db for wordpress
     sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', '$dbname');/" /var/www/wp-config.php
     sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', '$username');/" /var/www/wp-config.php
     sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '$password');/" /var/www/wp-config.php
 fi
-
-echo "CREATE DATABASE $dbname" | mysql
 
