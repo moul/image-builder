cf0ba731b2ae9cdc2bb787cf8d5855522101747f
Manfred Touron <m@42.am>
2014-10-29 17:09:51 +0100

    [pydio] Fixed mysql-server install

--- images/app-pydio/patches/usr/local/bin/install_mysql_server.sh
+++ images/app-pydio/patches/usr/local/bin/install_mysql_server.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 if [ ! -f /root/.my.cnf ]; then
+    username="root"
+    dbname="pydio"
     password=$(pwgen 42)
     cat <<EOF > /root/.my.cnf
 [client]
 user = root
 password = $password
 EOF
-    echo "mysql-server mysql-server/root_password password $password" debconf-set-selections
-    echo "mysql-server mysql-server/root_password_again password $password" debconf-set-selections
-    apt-get install -qy mysql-server
+
+    # Reset mysql root password
+    cat <<EOF > /tmp/recover_root_mysql.ini
+UPDATE mysql.user SET password=PASSWORD("$password") where User='$username';
+FLUSH PRIVILEGES;
+EOF
+    service mysql stop
+    mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
+    sleep 5
+    echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
+    killall mysqld
+    rm -f /tmp/recover_root_mysql.ini
+    service mysql start
 fi
-
-echo "CREATE DATABASE pydio" | mysql
-
