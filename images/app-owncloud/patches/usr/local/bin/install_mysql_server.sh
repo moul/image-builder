c063b49bdf950675406df540bddff0c61ab9f1b0
Manfred Touron <m@42.am>
2014-10-29 16:46:33 +0100

    Installing mysql-server earlier

--- images/app-owncloud/patches/usr/local/bin/install_mysql_server.sh
+++ images/app-owncloud/patches/usr/local/bin/install_mysql_server.sh
@@ -2,12 +2,25 @@
 
 if [ ! -f /root/.my.cnf ]; then
     password=$(pwgen 42)
+    dbname=owncloud
+    username="root"
     cat <<EOF > /root/.my.cnf
 [client]
-user = root
+user = $username
 password = $password
 EOF
-    echo "mysql-server mysql-server/root_password password $password" debconf-set-selections
-    echo "mysql-server mysql-server/root_password_again password $password" debconf-set-selections
-    apt-get install -qy mysql-server
+
+    # Reset mysql root password
+    cat <<EOF
+EOF > /tmp/recover_root_mysql.ini
+UPDATE mysql.user SET password=PASSWORD("$password") where User='$username';
+FLUSH PRIVILEGES;
+EOF
+    service mysql stop
+    mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
+    sleep 5
+    # echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
+    killall mysqld
+    rm -f /tmp/recover_root_mysql.ini
+    service mysql start
 fi
