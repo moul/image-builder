42e916932ea3c603915a45121b8eb206887a0de0
Manfred Touron <m@42.am>
2014-10-30 10:50:44 +0100

    [pydio] Fixed install_server_mysql.sh

--- images/app-pydio/patches/usr/local/bin/install_mysql_server.sh
+++ images/app-pydio/patches/usr/local/bin/install_mysql_server.sh
@@ -17,9 +17,16 @@ FLUSH PRIVILEGES;
 EOF
     service mysql stop
     mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
-    sleep 5
+
+    while ! [[ "$mysqld_process_pid" =~ ^[0-9]+$ ]]; do
+	mysqld_process_pid=$(echo "$(ps -C mysqld -o pid=)" | sed -e 's/^ *//g' -e 's/ *$//g')
+	sleep 1
+    done
+
     echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
-    killall mysqld
+
     rm -f /tmp/recover_root_mysql.ini
+    killall -w mysqld
+
     service mysql start
 fi
