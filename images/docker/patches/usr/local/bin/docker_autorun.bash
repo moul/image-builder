15add604547dd746d9016747b2a9325595cdb55c
Manfred Touron <m@42.am>
2014-11-04 23:59:14 +0100

    Fixed docker_autorun.bash script

--- images/docker/patches/usr/local/bin/docker_autorun.bash
+++ images/docker/patches/usr/local/bin/docker_autorun.bash
@@ -4,4 +4,6 @@ IMAGE="$1"
 ARGS="$2"
 OPTS="$3"
 
-docker start autorun-$IMAGE || docker run -d $OPTS -P --name autorun-$IMAGE $IMAGE $ARGS
+NAME=autorun-$(echo "$IMAGE" | tr "/" "-" | tr ":" "-")
+docker start $NAME || \
+    docker run -d $OPTS -P --name $NAME $IMAGE $ARGS
