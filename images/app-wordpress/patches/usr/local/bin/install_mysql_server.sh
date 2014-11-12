3a87db3234c4ca0ac50b41b07e991cfd4b5fcd39
Edouard Bonlieu <ebonlieusubs@gmail.com>
2014-11-12 14:34:12 +0100

    Replace killall -w with sleep

--- images/app-wordpress/patches/usr/local/bin/install_mysql_server.sh
+++ images/app-wordpress/patches/usr/local/bin/install_mysql_server.sh
@@ -17,15 +17,23 @@ FLUSH PRIVILEGES;
 EOF
     service mysql stop
     mysqld_safe --skip-grant-tables --init-file=/tmp/recover_root_mysql.ini &
-    sleep 5
-    echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
-    killall mysqld
-    rm -f /tmp/recover_root_mysql.ini
-    service mysql start
 
+    while ! [[ "$mysqld_process_pid" =~ ^[0-9]+$ ]]; do
+	    mysqld_process_pid=$(echo "$(ps -C mysqld -o pid=)" | sed -e 's/^ *//g' -e 's/ *$//g')
+	    sleep 1
+    done
+
+    echo "CREATE DATABASE $dbname" | mysql -u $username -p$password
+
+    rm -f /tmp/recover_root_mysql.ini
+    killall mysqld
+    sleep 5
+    killall -9 mysqld
+
+    service mysql start
+    
     # Configure wordpress database
     sed -i "s/define('DB_NAME',.*/define('DB_NAME', '$dbname');/" /var/www/wp-config.php
     sed -i "s/define('DB_USER',.*/define('DB_USER', '$username');/" /var/www/wp-config.php
     sed -i "s/define('DB_PASSWORD',.*/define('DB_PASSWORD', '$password');/" /var/www/wp-config.php
 fi
-
